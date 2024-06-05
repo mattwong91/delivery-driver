953332d52ecb72e779e73adbc463842caa7da165
Matthew Wong <144198400+mattwong91@users.noreply.github.com>
2024-06-05 01:15:14 -0600

    Rename script

--- Assets/Scripts/Delivery.cs
+++ Assets/Scripts/Delivery.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Delivery : MonoBehaviour
+public class Collision : MonoBehaviour
 {
   void OnCollisionEnter2D(Collision2D other)
   {

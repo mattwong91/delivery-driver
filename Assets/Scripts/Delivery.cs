476bcaca2c431d00a207a168ee344d12d5807227
Matthew Wong <144198400+mattwong91@users.noreply.github.com>
2024-06-05 01:58:26 -0600

    Change color of car on package pickup and delivery

--- Assets/Scripts/Delivery.cs
+++ Assets/Scripts/Delivery.cs
@@ -4,8 +4,18 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
+  [SerializeField] Color32 hasPackageColor = new Color32(1, 1, 1, 1);
+  [SerializeField] Color32 noPackageColor = new Color32(1, 1, 1, 1);
   [SerializeField] float destroyDelay = 1f;
+
   bool hasPackage = false;
+
+  SpriteRenderer spriteRenderer;
+
+  void Start()
+  {
+    spriteRenderer = GetComponent<SpriteRenderer>();
+  }
 
   void OnCollisionEnter2D(Collision2D other)
   {
@@ -18,12 +28,14 @@ public class Delivery : MonoBehaviour
     {
       Debug.Log("package picked up");
       hasPackage = true;
+      spriteRenderer.color = hasPackageColor;
       Destroy(other.gameObject, destroyDelay);
     }
     if (other.tag == "Customer" && hasPackage)
     {
       Debug.Log("You delivered the package!");
       hasPackage = false;
+      spriteRenderer.color = noPackageColor;
     }
   }
 }
